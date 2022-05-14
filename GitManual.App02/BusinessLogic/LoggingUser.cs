ccbcae33da8e0f6ee83c9f1211d246db4c162045
petavius <sebastian_z@wp.pl>
2022-05-14 16:41:17 +0200

    Dodano warstwę logiki biznesowej

---
+++ GitManual.App02/BusinessLogic/LoggingUser.cs
@@ -0,0 +1,11 @@
+﻿namespace GitManual.App02.BusinessLogic
+{
+    internal class LoggingUser
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ GitManual.App02/BusinessLogic/Sell.cs
@@ -0,0 +1,8 @@
+﻿namespace GitManual.App02.BusinessLogic
+{
+    internal class Sell
+    {
+        public decimal Price { get; set; }
+        public string ProductName { get; set; }
+    }
+}
