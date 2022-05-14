dd33c1e8be0c9acaba56429f8ff4f5c0c83fe4b9
petavius <sebastian_z@wp.pl>
2022-05-14 16:56:39 +0200

    Dodano warstwę Helpers

---
+++ GitManual.App02/Helpers/TextHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitManual.App02.Helpers
+{
+    internal class TextHelpers
+    {
+        public static void WriteSomething(string something)
+        {
+            MessageBox.Show($"Text to write: {something}");
+        }
+    }
+}
